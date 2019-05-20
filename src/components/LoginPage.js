810759da400c4bbce45002d952b971d2f39197a3
Dave Coble <davidcoble@gmail.com>
2019-05-20 09:33:12 -0700

    changing bg image

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -5,8 +5,8 @@ import { startLogin } from '../actions/auth';
 export const LoginPage = ({ startLogin }) => (
   <div className="box-layout">
     <div className="box-layout__box">
-      <h1 className="box-layout__title">Expensify</h1>
-      <p>It's time to get your expenses under control.</p>
+      <h1 className="box-layout__title">Dicey</h1>
+      <p>Roll virtual dice online and share your results.</p>
       <button className="button" onClick={startLogin}>Login with Google</button>
     </div>
   </div>
